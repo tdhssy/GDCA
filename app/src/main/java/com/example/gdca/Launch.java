062fb7df2554ebaac4973bed3c0108b8a4b6f76a
luffy <luffyvanquish@gmail.com>
2023-05-11 15:19:52 +0200

    Test GitLab

--- app/src/main/java/com/example/gdca/Launch.java
+++ app/src/main/java/com/example/gdca/Launch.java
@@ -3,19 +3,18 @@ package com.example.gdca;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.gdca.controller.ChangeView;
-import com.example.gdca.controller.MainController;
 import com.example.gdca.controller.PageName;
-import com.example.gdca.model.Model;
-import com.example.gdca.view.MainView;
+
 
 /**
  * Main class
+ * test
  */
 public class Launch extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        @SuppressWarnings("unused")
         ChangeView controller = new ChangeView(this, PageName.ACCEUIL);
     }
 
